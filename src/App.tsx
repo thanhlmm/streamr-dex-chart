e07d62bec44d04377cd3cef70b71dbc6404e48b7
Thanh Le <mihthanh27@gmail.com>
2021-11-17 21:50:29 +0700

    Fix can not build

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dex volume change</header>
-
       <DexVolume />
     </div>
   );
--- src/components/DexVolume.tsx
+++ src/components/DexVolume.tsx
@@ -1,72 +1,40 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { Subscription } from "streamr-client";
 import client from "../lib/streamr";
-import Chart from "react-apexcharts";
 import _ from "lodash";
-import { init, getInstanceByDom } from "echarts";
-import type { EChartsOption, ECharts, SetOptionOpts } from "echarts";
+import { init, getInstanceByDom, ECharts } from "echarts";
 
 const option = {
   title: {
     text: "Top 10 DEX by volume",
   },
-  tooltip: {
-    trigger: "axis",
+  legend: {
+    type: "plain",
   },
-  // legend: {
-  //   data: ["Email", "Union Ads", "Video Ads", "Direct", "Search Engine"],
-  // },
   grid: {
     left: "3%",
-    right: "4%",
+    right: "3%",
     bottom: "3%",
     containLabel: true,
   },
-  toolbox: {
-    feature: {
-      saveAsImage: {},
-    },
+  tooltip: {
+    order: "valueDesc",
+    trigger: "axis",
   },
   xAxis: {
     type: "time",
-    // boundaryGap: false,
-    // data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    splitLine: {
+      show: false,
+    },
   },
+  // xAxis: {
+  //   type: "category",
+  //   nameLocation: "middle",
+  // },
   yAxis: {
     type: "value",
+    boundaryGap: [0, "100%"],
   },
-  series: [
-    {
-      name: "Email",
-      type: "line",
-      // stack: "Total",
-      data: [120, 132, 101, 134, 90, 230, 210],
-    },
-    {
-      name: "Union Ads",
-      type: "line",
-      // stack: "Total",
-      data: [220, 182, 191, 234, 290, 330, 310],
-    },
-    {
-      name: "Video Ads",
-      type: "line",
-      // stack: "Total",
-      data: [150, 232, 201, 154, 190, 330, 410],
-    },
-    {
-      name: "Direct",
-      type: "line",
-      // stack: "Total",
-      data: [320, 332, 301, 334, 390, 330, 320],
-    },
-    {
-      name: "Search Engine",
-      type: "line",
-      // stack: "Total",
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
-    },
-  ],
 };
 
 const DexVolume = () => {
@@ -79,9 +47,15 @@ const DexVolume = () => {
     const sub = await client.subscribe(
       {
         stream: import.meta.env.VITE_STREAMR_STREAMID as string,
+        resend: {
+          last: 200,
+        },
       },
       (message, metadata) => {
-        setData(message);
+        const dataParsed = JSON.parse(message.data.data);
+        setData((data) => {
+          return [...data, [new Date(dataParsed[0].lastUpdated), dataParsed]];
+        });
         // TODO: Append data
       }
     );
@@ -104,7 +78,7 @@ const DexVolume = () => {
       {
         stream: import.meta.env.VITE_STREAMR_STREAMID as string,
         resend: {
-          last: 100,
+          last: 200,
         },
       },
       (message) => {
@@ -117,23 +91,30 @@ const DexVolume = () => {
     );
   };
 
-  useEffect(() => {
-    getLastData();
-  }, []);
+  // useEffect(() => {
+  //   getLastData();
+  // }, []);
 
   const categories = useMemo(() => {
     return _.uniq(
       data.reduce((prev, [_, dataPoint]) => {
         return [...prev, ...dataPoint.map((dexData: any) => dexData.slug)];
       }, [])
-    ).slice(1, 10);
+    ).slice(0, 10);
   }, [data]);
 
   const seriesData = useMemo(() => {
-    return categories.map((slug) => {
+    return categories.map((slug: string) => {
       return {
         name: slug,
         type: "line",
+        showSymbol: false,
+        labelLayout: {
+          moveOverlap: "shiftY",
+        },
+        emphasis: {
+          focus: "series",
+        },
         data: data.map(([date, dataRow]) => {
           const dexData = dataRow.find((dex: any) => dex.slug === slug);
           return [new Date(date).getTime(), dexData?.totalVol24h || 0];
@@ -171,12 +152,16 @@ const DexVolume = () => {
         series: seriesData,
       };
       if (chart) {
-        chart.setOption(chartOption);
+        chart.setOption(chartOption as any);
       }
     }
   }, [seriesData]);
 
-  return <div ref={chartRef} style={{ width: "100%", height: "600px" }} />;
+  return (
+    <div style={{ padding: 16 }}>
+      <div ref={chartRef} style={{ width: "100%", height: "600px" }} />
+    </div>
+  );
 };
 
 export default DexVolume;
